6de58f13122db41a59c8cfef801126187d9a0e0e
Keith <755170776@qq.com>
2019-08-19 14:36:40 +0800

    add leetcode-3:longestSubString
    2019.8.19 解决leetcode第三题：无重复字符串的最长子串

--- Java Solution/src/LongestSubstringWithoutRepeatingCharacters.java
+++ Java Solution/src/LongestSubstringWithoutRepeatingCharacters.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String args[]) {
 
-        String[] strings = {"dvdf"," ","abcabcbb", "bbbbb", "pwwkew"};
+        String[] strings = {"dvdf", " ", "abcabcbb", "bbbbb", "pwwkew","abba"};
         for (String s :
                 strings) {
             System.out.print(s + ":");
@@ -21,35 +21,31 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
     /**
      * 方法一：挨个将每个字符存入一个数据结构中
-     * 每次插入新的字符时判断之前是否出现过
-     * 如果出现过，则清空之前的数据结构，长度从现在开始重新算
-     * 如果没有出现过，则继续
      *
      * @param s
      * @return 最大不重复子串的长度
      */
     private static int lengthOfLongestSubstring(String s) {
-        //保存历史中，最长子串的长度
-        int maxLenth = 0;
+        int maxSubstringLength = 0, strLength = s.length();
 
-        //为了快速找到当前字符之前是否出现过，使用HashMap来存储
-        //HashMap中Key是s[i]，value取i
-        Map<Character,Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            Character c = s.charAt(i);
-            //如果当前字符重复了，则剔除掉上一个相同字符及之前的部分
-            if (hashMap.containsKey(c)){
-                //删除掉c字符和c之前的所有字符
-                int index = hashMap.get(c);
-                for (int j = 0; j < index; j++) {
-
-                }
+        //滑动窗口的起始
+        int start = 0, end = 0;
+        //key  :    s中的每个字符
+        //value:    该字符在s中的索引
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < strLength; i++) {
+            char ch = s.charAt(i);
+            end = i;
+            //从[start,end]之间有出现过该字符则滑动
+            if (map.containsKey(ch)) {
+                //abba 当判断最后一个a时，start在第二个b 也就是index=2，而map.get(a)=0,所以要选择最靠后（最新的）的start（也即2）
+                start = Math.max(start,map.get(ch) + 1);
             }
-            //更新maxLenth
-            //不管是否重复，都要更新一下最新的maxLenth，并将最新的字符存入HashMap
-            hashMap.put(c,i);
-            maxLenth = Math.max(maxLenth,hashMap.size());
+            maxSubstringLength = Math.max(maxSubstringLength, end - start + 1);
+
+            map.put(ch, i);
         }
-        return maxLenth;
+
+        return maxSubstringLength;
     }
 }
