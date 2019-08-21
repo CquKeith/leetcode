4a0e68d2c7673a4ce212d5602d7561d7f31773dc
Keith <755170776@qq.com>
2019-08-21 18:41:46 +0800

    add huawei 2016 deletenumbers,stringset

---
+++ Java Solution/src/Huawei2016/StringSet.java
@@ -0,0 +1,27 @@
+package Huawei2016;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * @author Keith
+ * @date 8/21/2019 16:38
+ **/
+public class StringSet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            Set<Character> set = new HashSet<>();
+            String s = scanner.nextLine();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Character item:
+                 s.toCharArray()) {
+                if (set.add(item)){
+                    stringBuilder.append(item);
+                }
+            }
+            System.out.println(stringBuilder.toString());
+        }
+    }
+}
