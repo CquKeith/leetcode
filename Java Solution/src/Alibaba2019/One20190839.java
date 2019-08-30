d98fb22d53ce200e49ba2312518754ac467565ef
Keith <755170776@qq.com>
2019-08-30 18:49:04 +0800

    init

---
+++ Java Solution/src/Alibaba2019/One20190839.java
@@ -0,0 +1,9 @@
+package Alibaba2019;
+
+/**
+ * @author Keith
+ * @date 8/30/2019 18:30
+ **/
+public class One20190839 {
+
+}
---
+++ Java Solution/src/Alibaba2019/Two20190830.java
@@ -0,0 +1,9 @@
+package Alibaba2019;
+
+/**
+ * @author Keith
+ * @date 8/30/2019 18:30
+ **/
+public class Two20190830 {
+
+}
