fa4f62990a78c834a0ee1ff3c11dcda361a40da8
Keith <755170776@qq.com>
2020-03-15 14:32:06 +0800

    Create InOrder.java
    二叉树中序遍历，递归和非递归实现
    非递归需要借助栈

---
+++ Java Solution/test/TreeTraverse/InOrder.java
@@ -0,0 +1,61 @@
+package TreeTraverse;
+
+import Topic.Tree.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * @author chenmengliang
+ * @date 2019/11/22 22:14
+ **/
+public class InOrder {
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.createTree(new Integer[]{1,2,3,4,5,6,7});
+
+        inOrderRecursive(root);
+        System.out.println();
+        inOrderNotRecursive(root);
+
+    }
+
+    /**
+     * 递归中序遍历
+     * @param root
+     */
+    public static void inOrderRecursive(TreeNode root){
+        if (null != root){
+            inOrderRecursive(root.left);
+            System.out.print(root.val+" ");
+            inOrderRecursive(root.right);
+        }
+    }
+
+    /**
+     * 非递归中序遍历
+     * 借助栈，找到最左边的叶子节点之后，将上一层弹出，然后访问右节点
+     * @param root
+     */
+    public static void inOrderNotRecursive(TreeNode root){
+
+        if (null == root)return;
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.isEmpty()){
+
+            if (root != null){
+                stack.push(root);
+                root = root.left;//当前节点不为空，就往左走
+            }else{// 遇到叶子节点了
+                root = stack.pop();
+                System.out.print(root.val+" ");
+                root = root.right;//往右走
+            }
+
+
+        }
+
+
+
+    }
+}
